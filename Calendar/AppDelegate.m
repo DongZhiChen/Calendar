1a4181cbb94538a3d33a2c6ad25756c585f5198c
DongZhiChen <644495218@qq.com>
2017-06-27 06:10:26 +0800

    master1_1

--- Calendar/AppDelegate.m
+++ Calendar/AppDelegate.m
@@ -16,7 +16,6 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    ///branch 1_2
     // Override point for customization after application launch.
     return YES;
 }
